b69dfa9521a3f46f98336e8828187892fa171151
Brooks Palin <brookspalin710@gmail.com>
2023-01-09 18:33:30 -0700

    added clone functionallity

--- backend/src/blog.rs
+++ backend/src/blog.rs
@@ -1,10 +1,22 @@
-use std::{io, path};
+use std::{path};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct BlogRef {
     id : usize,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct BlogMeta {
+    tags : Vec<String>
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Blog {
+    id : usize,
+    meta : BlogMeta,
+    content : String
 }
 
 impl BlogRef {
@@ -15,22 +27,47 @@ impl BlogRef {
         }
     }
 
+    pub fn load_blog() -> Blog {}
+
+    pub fn load_meta(&self) -> Option<BlogMeta> {
+        if let Some(mut path) = self.get_path() {
+            path.push("meta.json");
+            let content = std::fs::read_to_string(path);
+            if let Ok(content) = content {
+                let meta = serde_json::from_str::<BlogMeta>(content.as_str());
+                if let Ok(meta) = meta {
+                    return Some(meta);
+                }
+            }
+        }
+
+        None
+    }
+
     pub fn load_md(&self) -> Option<String> {
+        if let Some(mut path) = self.get_path() {
+            path.push("content.md");
+            let content = std::fs::read_to_string(path);
+            if let Ok(content) = content {
+                return Some(content)
+            }
+        }
+                
+        None
+    }
+
+    pub fn get_path(&self) -> Option<path::PathBuf>{
         let paths = std::fs::read_dir("content/blogs");
 
         if let Ok(mut dir) = paths {
             if let Some(path) = dir.nth(self.id) {
                 if let Ok(blog) = path {
-                    let mut buf = blog.path();
-                    buf.push("content.md");
-                    let content = std::fs::read_to_string(buf);
-                    if let Ok(content) = content {
-                        return Some(content)
-                    }
+                    let buf = blog.path();
+                    return Some(buf);
                 }
             }
         }
-        
+
         None
     }
 }
---
+++ backend/src/git.rs
@@ -0,0 +1,286 @@
+use git2::{Repository, Progress, RemoteCallbacks, build::{CheckoutBuilder, RepoBuilder}, FetchOptions};
+use std::{io::{self, Write}, path::{PathBuf, Path}, cell::RefCell};
+
+const remote_url : &str = "https://github.com/MrVintage710/personal-site-content.git";
+const remote_path: &str = "./content";
+const remote_name : &str = "origin";
+const remote_branch : &str = "main";
+
+//Probably not the most secure FML
+pub fn pull() {
+    println!("\nStarting new Pull!");
+    let repo = get_repo();
+
+    let mut remote = repo.find_remote(remote_name).expect("Error: Remote 'Origin' doesn't exist.");
+
+    let fetch_commit = do_fetch(&repo, &[remote_branch], &mut remote).expect("Unable to fetch changes");
+
+    do_merge(&repo, remote_branch, fetch_commit).expect("Unable to pull from the branch.")
+}
+
+fn get_repo() -> Repository {
+    match Repository::open(remote_path){
+        Ok(repo) => repo,
+        Err(_) => clone_repo(),
+    }
+}
+
+fn clone_repo() -> Repository {
+    println!("  Starting new Clone");
+    clone()
+}
+
+//This is an a Example from the git2 repo
+//https://github.com/rust-lang/git2-rs/blob/master/examples/pull.rs
+fn do_fetch<'a>(
+    repo: &'a git2::Repository,
+    refs: &[&str],
+    remote: &'a mut git2::Remote,
+) -> Result<git2::AnnotatedCommit<'a>, git2::Error> {
+    let mut cb = git2::RemoteCallbacks::new();
+
+    // Print out our transfer progress.
+    cb.transfer_progress(|stats| {
+        if stats.received_objects() == stats.total_objects() {
+            print!(
+                "  Resolving deltas {}/{}\r",
+                stats.indexed_deltas(),
+                stats.total_deltas()
+            );
+        } else if stats.total_objects() > 0 {
+            print!(
+                "  Received {}/{} objects ({}) in {} bytes\r",
+                stats.received_objects(),
+                stats.total_objects(),
+                stats.indexed_objects(),
+                stats.received_bytes()
+            );
+        }
+        io::stdout().flush().unwrap();
+        true
+    });
+
+    let mut fo = git2::FetchOptions::new();
+    fo.remote_callbacks(cb);
+    // Always fetch all tags.
+    // Perform a download and also update tips
+    fo.download_tags(git2::AutotagOption::All);
+    println!("  Fetching {} for repo", remote.name().unwrap());
+    remote.fetch(refs, Some(&mut fo), None)?;
+
+    // If there are local objects (we got a thin pack), then tell the user
+    // how many objects we saved from having to cross the network.
+    let stats = remote.stats();
+    if stats.local_objects() > 0 {
+        println!(
+            "\r  Received {}/{} objects in {} bytes (used {} local \
+             objects)",
+            stats.indexed_objects(),
+            stats.total_objects(),
+            stats.received_bytes(),
+            stats.local_objects()
+        );
+    } else {
+        println!(
+            "\r  Received {}/{} objects in {} bytes",
+            stats.indexed_objects(),
+            stats.total_objects(),
+            stats.received_bytes()
+        );
+    }
+
+    let fetch_head = repo.find_reference("FETCH_HEAD")?;
+    Ok(repo.reference_to_annotated_commit(&fetch_head)?)
+}
+
+fn fast_forward(
+    repo: &Repository,
+    lb: &mut git2::Reference,
+    rc: &git2::AnnotatedCommit,
+) -> Result<(), git2::Error> {
+    let name = match lb.name() {
+        Some(s) => s.to_string(),
+        None => String::from_utf8_lossy(lb.name_bytes()).to_string(),
+    };
+    let msg = format!("  Fast-Forward: Setting {} to id: {}", name, rc.id());
+    println!("{}", msg);
+    lb.set_target(rc.id(), &msg)?;
+    repo.set_head(&name)?;
+    repo.checkout_head(Some(
+        git2::build::CheckoutBuilder::default()
+            // For some reason the force is required to make the working directory actually get updated
+            // I suspect we should be adding some logic to handle dirty working directory states
+            // but this is just an example so maybe not.
+            .force(),
+    ))?;
+    Ok(())
+}
+
+fn normal_merge(
+    repo: &Repository,
+    local: &git2::AnnotatedCommit,
+    remote: &git2::AnnotatedCommit,
+) -> Result<(), git2::Error> {
+    let local_tree = repo.find_commit(local.id())?.tree()?;
+    let remote_tree = repo.find_commit(remote.id())?.tree()?;
+    let ancestor = repo
+        .find_commit(repo.merge_base(local.id(), remote.id())?)?
+        .tree()?;
+    let mut idx = repo.merge_trees(&ancestor, &local_tree, &remote_tree, None)?;
+
+    if idx.has_conflicts() {
+        println!("  Merge conflicts detected...");
+        repo.checkout_index(Some(&mut idx), None)?;
+        return Ok(());
+    }
+    let result_tree = repo.find_tree(idx.write_tree_to(repo)?)?;
+    // now create the merge commit
+    let msg = format!("  Merge: {} into {}", remote.id(), local.id());
+    let sig = repo.signature()?;
+    let local_commit = repo.find_commit(local.id())?;
+    let remote_commit = repo.find_commit(remote.id())?;
+    // Do our merge commit and set current branch head to that commit.
+    let _merge_commit = repo.commit(
+        Some("HEAD"),
+        &sig,
+        &sig,
+        &msg,
+        &result_tree,
+        &[&local_commit, &remote_commit],
+    )?;
+    // Set working tree to match head.
+    repo.checkout_head(None)?;
+    Ok(())
+}
+
+fn do_merge<'a>(
+    repo: &'a Repository,
+    remote_branch_var: &'a str,
+    fetch_commit: git2::AnnotatedCommit<'a>,
+) -> Result<(), git2::Error> {
+    // 1. do a merge analysis
+    let analysis = repo.merge_analysis(&[&fetch_commit])?;
+
+    // 2. Do the appropriate merge
+    if analysis.0.is_fast_forward() {
+        println!("  Doing a fast forward");
+        // do a fast forward
+        let refname = format!("refs/heads/{}", remote_branch_var);
+        match repo.find_reference(&refname) {
+            Ok(mut r) => {
+                fast_forward(repo, &mut r, &fetch_commit)?;
+            }
+            Err(_) => {
+                // The branch doesn't exist so just set the reference to the
+                // commit directly. Usually this is because you are pulling
+                // into an empty repository.
+                repo.reference(
+                    &refname,
+                    fetch_commit.id(),
+                    true,
+                    &format!("  Setting {} to {}", remote_branch_var, fetch_commit.id()),
+                )?;
+                repo.set_head(&refname)?;
+                repo.checkout_head(Some(
+                    git2::build::CheckoutBuilder::default()
+                        .allow_conflicts(true)
+                        .conflict_style_merge(true)
+                        .force(),
+                ))?;
+            }
+        };
+    } else if analysis.0.is_normal() {
+        // do a normal merge
+        let head_commit = repo.reference_to_annotated_commit(&repo.head()?)?;
+        normal_merge(&repo, &head_commit, &fetch_commit)?;
+    } else {
+        println!("  Nothing to do...");
+    }
+    Ok(())
+}
+
+struct State {
+    progress: Option<Progress<'static>>,
+    total: usize,
+    current: usize,
+    path: Option<PathBuf>,
+    newline: bool,
+}
+
+fn print(state: &mut State) {
+    let stats = state.progress.as_ref().unwrap();
+    let network_pct = (100 * stats.received_objects()) / stats.total_objects();
+    let index_pct = (100 * stats.indexed_objects()) / stats.total_objects();
+    let co_pct = if state.total > 0 {
+        (100 * state.current) / state.total
+    } else {
+        0
+    };
+    let kbytes = stats.received_bytes() / 1024;
+    if stats.received_objects() == stats.total_objects() {
+        if !state.newline {
+            println!();
+            state.newline = true;
+        }
+        print!(
+            "    Resolving deltas {}/{}\r",
+            stats.indexed_deltas(),
+            stats.total_deltas()
+        );
+    } else {
+        print!(
+            "    net {:3}% ({:4} kb, {:5}/{:5})  /  idx {:3}% ({:5}/{:5})  \
+             /  chk {:3}% ({:4}/{:4}) {}\r",
+            network_pct,
+            kbytes,
+            stats.received_objects(),
+            stats.total_objects(),
+            index_pct,
+            stats.indexed_objects(),
+            stats.total_objects(),
+            co_pct,
+            state.current,
+            state.total,
+            state
+                .path
+                .as_ref()
+                .map(|s| s.to_string_lossy().into_owned())
+                .unwrap_or_default()
+        )
+    }
+    io::stdout().flush().unwrap();
+}
+
+fn clone() -> Repository {
+    let state = RefCell::new(State {
+        progress: None,
+        total: 0,
+        current: 0,
+        path: None,
+        newline: false,
+    });
+    let mut cb = RemoteCallbacks::new();
+    cb.transfer_progress(|stats| {
+        let mut state = state.borrow_mut();
+        state.progress = Some(stats.to_owned());
+        print(&mut *state);
+        true
+    });
+
+    let mut co = CheckoutBuilder::new();
+    co.progress(|path, cur, total| {
+        let mut state = state.borrow_mut();
+        state.path = path.map(|p| p.to_path_buf());
+        state.current = cur;
+        state.total = total;
+        print(&mut *state);
+    });
+
+    let mut fo = FetchOptions::new();
+    fo.remote_callbacks(cb);
+    let repo = RepoBuilder::new()
+        .fetch_options(fo)
+        .with_checkout(co)
+        .clone(remote_url, Path::new(remote_path)).expect("Unable to build Repo");
+    repo
+}
